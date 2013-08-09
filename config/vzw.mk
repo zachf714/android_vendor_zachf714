e60ea90430766bf74e64773f383391d4e1e4aa72
zachf714 <zachf714@gmail.com>
2013-08-09 18:50:38 -0400

    Add VZW Prebuilts

---
+++ config/vzw.mk
@@ -0,0 +1,9 @@
+#Permissions and Lib for Verizon Wireless Apps 
+
+PRODUCT_COPY_FILES += \
+    vendor/zachf714/prebuilt/vzw/app/VerizonSSO.apk:system/app/VerizonSSO.apk \
+    vendor/zachf714/prebuilt/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
+    vendor/zachf714/prebuilt/vzw/etc/permissions/com.vzw.hardware.lte.xml:system/etc/permissions/com.vzw.hardware.lte.xml \
+    vendor/zachf714/prebuilt/vzw/etc/permissions/com.vzw.vzwapnlib.xml:system/etc/permissions/com.vzw.vzwapnlib.xml \
+    vendor/zachf714/prebuilt/vzw/lib/libmotricity.so:system/lib/libmotricity.so
+
