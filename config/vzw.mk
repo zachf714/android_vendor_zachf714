0d10e0f4713e2b854ca7fbcceb5165bb8bfe6bae
zachf714 <zachf714@gmail.com>
2013-11-19 21:51:27 -0500

    ROMDB

--- config/vzw.mk
+++ config/vzw.mk
@@ -2,6 +2,7 @@
 
 PRODUCT_COPY_FILES += \
     vendor/zachf714/prebuilt/vzw/app/VerizonSSO.apk:system/app/VerizonSSO.apk \
+    vendor/zachf714/prebuilt/vzw/app/ROMdBDevTool-v1.0.7.apk:system/app/ROMdBDevTool-v1.0.7.apk \
     vendor/zachf714/prebuilt/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
     vendor/zachf714/prebuilt/vzw/etc/permissions/com.vzw.hardware.lte.xml:system/etc/permissions/com.vzw.hardware.lte.xml \
     vendor/zachf714/prebuilt/vzw/etc/permissions/com.vzw.vzwapnlib.xml:system/etc/permissions/com.vzw.vzwapnlib.xml \
